1fca4ea46d777fee227a2e1dfe39ffb6a9823c1f
iamilyfelix <iamilyfelix6@gmail.com>
2026-04-02 05:08:47 -0300

    Initial commit

--- main.js
+++ main.js
@@ -1,107 +1,208 @@
-function menu() {
-    while (true) {
-        var opcao = prompt("INFORME SUA OPÇÃO\n 1 - Cadastrar cliente\n 2 - Listar clientes\n 3 - Buscar dados do cliente\n 4 - Editar dados do cliente\n 5 - Remover cliente");
+document.getElementById("btnCadastrar").addEventListener("click", cadastrarCliente);
+document.getElementById("buscarNome").addEventListener("click", buscarCliente);
+document.querySelectorAll(".excluirCliente").forEach(botao => {
+    botao.addEventListener("click", () => {
+        const id = botao.getAttribute("data-id");
+        excluirCliente(id);
+    });
+});
 
-        if (opcao === "1") {
-            cadastrarCliente();
-        } else if (opcao === "2") {
-            listarClientes();
-        } else if (opcao === "3") {
-            buscarDados();
-        } else if (opcao === "4") {
-            editarCliente();
-        } else if (opcao === "5") {
-            excluirCliente();
-        } else {
-            alert("opção invalida!")
-        }
-
+document.getElementById("pesquisarNome").addEventListener("input", (e) => {
+    const valor = e.target.value.trim();
+    if (valor === "") {
+        exibirClientes(); // recarrega todos os clientes
     }
+});
+
+function abrirModalEdicao(cliente) {
+    
+    document.getElementById("editId").value = cliente.id;
+    document.getElementById("editNome").value = cliente.nome;
+    document.getElementById("editTelefone").value = cliente.telefone;
+    document.getElementById("editEmail").value = cliente.email;
+    document.getElementById("editCPF").value = cliente.cpf;
+
+    // Abre o modal
+    const modal = new bootstrap.Modal(document.getElementById("modalEditarCliente"));
+    modal.show();
+
+    // adiciona o listener do botao salvar aqui dentro
+    document.getElementById("salvarEdicao").onclick = function () {
+        const id = document.getElementById("editId").value;
+        const nome = document.getElementById("editNome").value;
+        const telefone = document.getElementById("editTelefone").value;
+        const email = document.getElementById("editEmail").value;
+        const cpf = document.getElementById("editCPF").value;
+
+        fetch(`https://688a495f4c55d5c73955c4e8.mockapi.io/cliente/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ nome, telefone, email, cpf })
+        })
+            .then(res => res.json())
+            .then(() => {
+                alert("Dados alterados com sucesso!");
+                const modal = bootstrap.Modal.getInstance(document.getElementById("modalEditarCliente"));
+                modal.hide();
+                exibirClientes(); 
+            })
+            .catch(err => {
+                alert("Erro ao editar cliente.");
+                console.error(err);
+            });
+    };
 }
+
+window.onload = exibirClientes;
+
 function cadastrarCliente() {
-    const nome = prompt("Nome: ");
-    const telefone = prompt("Telefone: ")
-    const email = prompt("E-mail: ");
-    const cpf = prompt("CPF: ");
+    const nome = document.getElementById("nomeCliente").value;
+    const telefone = document.getElementById("telCliente").value;
+    const email = document.getElementById("emailCliente").value;
+    const cpf = document.getElementById("cpfCliente").value;
 
-    clientes.push({ nome, telefone, email, cpf });
-
-    alert("Cliente cadastrado!")
+    fetch('https://688a495f4c55d5c73955c4e8.mockapi.io/cliente', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            telefone: telefone,
+            email: email,
+            cpf: cpf
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            alert('Cliente cadastrado com sucesso!');
+            const modal = bootstrap.Modal.getInstance(document.getElementById("meuModal"));
+            modal.hide();
+            exibirClientes();
+            limparFormulario();
+        })
+        .catch(error => {
+            alert('Erro ao cadastrar cliente.', error);
+        });
 }
 
-function listarClientes() {
-    let listarClientes = "";
-    for (let i = 0; i < clientes.length; i++) {
-
-        listarClientes += "Nome: " + clientes[i].nome +
-            "\nTelefone: " + clientes[i].telefone +
-            "\nE-mail: " + clientes[i].email +
-            "\nCPF: " + clientes[i].cpf +
-            "\n--------------------------\n"
-    }
-
-    alert(listarClientes);
-}
-
-function buscarDados() {
-    const buscarcpf = prompt("Digite o CPF do cliente:");
-
-    let cliente = clientes.find(pessoa => pessoa.cpf === buscarcpf);
-
-    if (cliente) {
-        alert(
-            "Nome: " + cliente.nome +
-            "\nTelefone: " + cliente.telefone +
-            "\nE-mail: " + cliente.email +
-            "\nCPF: " + cliente.cpf
-        );
-    } else {
-        alert("Cliente não encontrado!");
-    }
+function limparFormulario() {
+    document.getElementById("nomeCliente").value = "";
+    document.getElementById("telCliente").value = "";
+    document.getElementById("emailCliente").value = "";
+    document.getElementById("cpfCliente").value = "";
 }
 
 
-function editarCliente() {
-    const cpfBusca = prompt("Digite o CPF do cliente que deseja editar:");
-    const cliente = clientes.find(c => c.cpf.toLowerCase() === cpfBusca.toLowerCase());
+function exibirClientes() {
+    const corpoTabela = document.getElementById("corpoTabela");
+    corpoTabela.innerHTML = ''; // limpa o corpo da tabela antes de exibir os dados
 
-    if (!cliente) {
-        alert("Cliente não encontrado.");
-        return;
-    }
+    fetch('https://688a495f4c55d5c73955c4e8.mockapi.io/cliente')
+        .then(res => res.json())
+        .then(clientes => {
+            clientes.forEach(cliente => {
+                const linha = document.createElement("tr");
+                linha.innerHTML = `
+                    <td>${cliente.nome}</td>
+                    <td>${cliente.telefone}</td>
+                    <td>${cliente.email}</td>
+                    <td>${cliente.cpf}</td>
+                    <td><button class="btn btn-primary editarCliente">Editar</button></td>
+                    <td><button class="btn btn-danger excluirCliente">Excluir</button></td>
+                `;
 
-    const novoNome = prompt("Novo nome:", cliente.nome);
-    const novoTelefone = prompt("Novo telefone:", cliente.telefone);
-    const novoEmail = prompt("Novo email:", cliente.email);
-    const novoCpf = prompt("Novo CPF:", cliente.cpf);
+                // Adiciona o evento ao botão de excluir
+                const botaoExcluir = linha.querySelector(".excluirCliente");
+                botaoExcluir.addEventListener("click", () => {
+                    excluirCliente(cliente.id);
+                });
 
-    cliente.nome = novoNome || cliente.nome;
-    cliente.telefone = novoTelefone || cliente.telefone;
-    cliente.email = novoEmail || cliente.email;
-    cliente.cpf = novoCpf || cliente.cpf;
 
-    alert("Cliente atualizado!");
+                // botão editar com função separada
+                const botaoEditar = linha.querySelector(".editarCliente");
+                botaoEditar.addEventListener("click", () => {
+                    abrirModalEdicao(cliente);
+                });
+
+                corpoTabela.appendChild(linha); 
+            });
+        })
+        .catch(erro => alert('Erro ao buscar:', erro));
 }
 
-function excluirCliente() {
-    if (clientes.length === 0) {
-        alert("Nenhum cliente cadastrado.");
-        return;
-    }
 
-    const cpf = prompt("Digite o CPF do cliente que deseja excluir:");
-    const index = clientes.findIndex(c => c.cpf === cpf);
+function buscarCliente() {
+    const nomeBuscado = document.getElementById('pesquisarNome').value.toLowerCase();
+    const corpoTabela = document.getElementById("corpoTabela");
+    corpoTabela.innerHTML = '';
 
-    if (index === -1) {
-        alert("Cliente com esse CPF não foi encontrado.");
-        return;
-    }
+    fetch('https://688a495f4c55d5c73955c4e8.mockapi.io/cliente')
+        .then(response => response.json())
+        .then(clientes => {
+            // filtra os clientes 
+            const clientesFiltrados = clientes.filter(c => c.nome.toLowerCase() === nomeBuscado);
 
-    const confirmar = confirm(`Tem certeza que deseja excluir ${clientes[index].nome}?`);
-    if (confirmar) {
-        clientes.splice(index, 1);
-        alert("Cliente excluído com sucesso!");
+            if (clientesFiltrados.length === 0) {
+                alert = 'Nenhum cliente encontrado.';
+                return;
+            }
+
+            clientesFiltrados.forEach(cliente => {
+                const linha = document.createElement("tr");
+
+                linha.innerHTML = `
+                    <td>${cliente.nome}</td>
+                    <td>${cliente.telefone}</td>
+                    <td>${cliente.email}</td>
+                    <td>${cliente.cpf}</td>
+                    <td><button class="btn btn-primary editarCliente">Editar</button></td>
+                    <td><button class="btn btn-danger excluirCliente">Excluir</button></td>
+                `;
+                //botao excluir
+                const botaoExcluir = linha.querySelector(".excluirCliente");
+                botaoExcluir.addEventListener("click", () => {
+                    excluirCliente(cliente.id);
+                    exibirClientes();
+                });
+
+
+                // botão editar 
+                const botaoEditar = linha.querySelector(".editarCliente");
+                botaoEditar.addEventListener("click", () => {
+                    abrirModalEdicao(cliente);
+                    exibirClientes();
+                });
+
+                corpoTabela.appendChild(linha)
+            });
+        })
+        .catch(error => {
+            alert('Erro ao buscar cliente.', error);
+        });
+
+}
+
+function excluirCliente(id) {
+    if (confirm("Tem certeza que deseja excluir este cliente?")) {
+        fetch(`https://688a495f4c55d5c73955c4e8.mockapi.io/cliente/${id}`, {
+            method: "DELETE"
+        })
+            .then(res => res.json())
+            .then(() => {
+                alert("Cliente excluído com sucesso!");
+                exibirClientes(); 
+            })
+            .catch(err => {
+                alert("Erro ao excluir cliente.");
+                console.error(err);
+            });
     }
 }
+
+
+
 
 
